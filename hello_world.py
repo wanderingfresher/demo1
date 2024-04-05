4d06088b1136db45145763aaeeda3a21cff3d56f
wanderingfresher <leeyunbai2@gmail.com>
2024-04-06 02:41:52 +0800

    some extra codes

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,31 @@
-001
+import numpy
+import time
+print(time.time)
+print(numpy.arange(10))
+for i in range(10):
+    print(i)
+
+while True:
+    print("hello")
+    time.sleep(1)
+
+
+def dynamicProgramming(n):
+    """
+    A function that calculates the n-th Fibonacci number using dynamic programming.
+
+    Parameters:
+    n (int): The index of the Fibonacci sequence.
+
+    Returns:
+    int: The n-th Fibonacci number.
+    """
+    if n == 0:  # Base case: Fibonacci(0) = 0
+        return 0
+    elif n == 1:  # Base case: Fibonacci(1) = 1
+        return 1
+    else:  # Recursive case: Fibonacci(n) = Fibonacci(n-1) + Fibonacci(n-2)
+        return dynamicProgramming(n - 1) + dynamicProgramming(n - 2)
+
+
+
